e6a6b826a339b06a119a1da87964eb38667fca34
Balamurali-Gif <71128841+Balamurali-Gif@users.noreply.github.com>
2021-01-25 09:16:29 +0000

    Add files via upload

---
+++ HolidaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  HolidaysApp.swift
+//  Holidays
+//
+//  Created by Bala Murali on 22/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct HolidaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
